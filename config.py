88275463d3c95cb5fb70483092f29c32b537c6a8
𝐊𝐚𝐧𝐡𝐚 <ayush03kmr@gmail.com>
2026-04-25 22:22:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ API_HASH = getenv("API_HASH", "4c246fb0c649477cc2e79b6a178ddfaa")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 
 OWNER_ID = int(getenv("OWNER_ID", "8508338965"))
+OWNER_USERNAME = getenv("OWNER_USERNAME", "KanhaaOp")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "")
 MUST_JOIN = getenv("MUST_JOIN", "AnupriyaUpdate")
 START_IMG = getenv("START_IMG", "https://i.ibb.co/DfVvdjH7/x.jpg")
